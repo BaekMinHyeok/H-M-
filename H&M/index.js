08afd4f70dbf4c28066f158f637eb9e5f18b295a
BaekMinHyeok <minhyeokbaek100@gmail.com>
2021-10-29 22:01:12 +0900

    2021-10-29 작업

--- H&M/index.js
+++ H&M/index.js
@@ -1,16 +1,18 @@
 
-$(document).ready(function(){
-    var $firstmenu = $('top-menu>menu>ul>li>a'),
-        $header = $('top-bar');
-     $firstmenu.mouseenter(function(){
-        $header.stop().animate({height:'300px'},200);
-     })
-     .mouseleave(function(){
-         $header.stop().animate({height:'50px'},200);
-     })
+// $(document).ready(function(){
+//     var $firstmenu = $('top-menu>menu>ul>li>a'),
+//         $header = $('top-bar');
+//      $firstmenu.mouseenter(function(){
+//         $header.stop().animate({height:'550px'},200);
+//      })
+//      .mouseleave(function(){
+//          $header.stop().animate({height:'50px'},200);
+//      })
      
      
- });
+//  });
+
+
 
  $(document).ready(function(){
     $('.main-slider').slick({
@@ -18,6 +20,9 @@ $(document).ready(function(){
       autoplay: true,
       autoplaySpeed: 5000,
       fade: false,
-    //   pauseOnHover: false,
+      slidesToShow: 1,   
+     slidesToScroll: 1,
+     fade: true,
+     pauseOnHover: true,
     });
 });
