8232edabdc30948b70acd60cb833acdc845418d8
BaekMinHyeok <minhyeokbaek100@gmail.com>
2021-10-31 16:38:08 +0900

    2021-10-31 작업

--- H&M/index.js
+++ H&M/index.js
@@ -21,8 +21,29 @@
       autoplaySpeed: 5000,
       fade: false,
       slidesToShow: 1,   
-     slidesToScroll: 1,
-     fade: true,
-     pauseOnHover: true,
+      slidesToScroll: 1,
+      fade: true,
+      pauseOnHover: true,
+      prevArrow: $(".btn-wrap .prev"),
+      nextArrow: $(".btn-wrap .next"),
+      customPaging: function (slick, index) {
+        return '<div class="page-btn"></div>'
+      }
     });
+
+    $('.category-box .woman').mouseover(function(){
+      $('.category-box .img-box').css('background-image','url(../H&M/img/section2/Kategorie.jpeg)') .css('transition','0.5s') ;
+    });
+    $('.category-box .woman').mouseout(function(){
+      $('.category-box .img-box').css('background-image','url(https://hmgroup.com/wp-content/uploads/2021/02/HM-REMAKE-FINAL_5813-scaled.jpg)') .css('transition','all 0.5s');
+    });
+
+
+
+
+    
+
+
 });
+
+
