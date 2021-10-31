ea6b9c7b603d906f236116a51936cf8ef6592b7a
BaekMinHyeok <minhyeokbaek100@gmail.com>
2021-11-01 03:29:41 +0900

    2021-11-01 작업

--- H&M/index.js
+++ H&M/index.js
@@ -19,7 +19,6 @@
       dots: true,
       autoplay: true,
       autoplaySpeed: 5000,
-      fade: false,
       slidesToShow: 1,   
       slidesToScroll: 1,
       fade: true,
@@ -31,17 +30,132 @@
       }
     });
 
+
+    // 카테고리 박스01
+
     $('.category-box .woman').mouseover(function(){
-      $('.category-box .img-box').css('background-image','url(../H&M/img/section2/Kategorie.jpeg)') .css('transition','0.5s') ;
+      $('.category-box .img-box').css({
+        "background-image":"url(../H&M/img/section2/Kategorie.jpeg)",
+        "transition":"0.5s",
+        "background-repeat":"no-repeat",
+        "background-size":"cover",
+        "background-position":"center",
+        "position":"relative",
+        "width: 100%":"height: 100%",
+      });
+
     });
     $('.category-box .woman').mouseout(function(){
-      $('.category-box .img-box').css('background-image','url(https://hmgroup.com/wp-content/uploads/2021/02/HM-REMAKE-FINAL_5813-scaled.jpg)') .css('transition','all 0.5s');
+      $('.category-box .img-box').css({
+        "background-image":"url(../H&M/img/section2/h&m-main.jpg)",
+        "background-repeat":"no-repeat",
+        "background-size":"cover",
+        "background-position":"center",
+        "position":"relative",
+        "width: 100%":"height: 100%",
+
+
+      });
+
     });
 
-
-
-
+    // 카테고리박스02
     
+    $('.category-box .man').mouseover(function(){
+      $('.category-box .img-box').css({
+        "background-image":"url(../H&M/img/section2/man1.jpg)",
+        "transition":"0.5s",
+        "background-repeat":"no-repeat",
+        "background-size":"cover",
+        "background-position":"center",
+        "position":"relative",
+        "width: 100%":"height: 100%",
+      });
+
+    });
+    $('.category-box .man').mouseout(function(){
+      $('.category-box .img-box').css({
+        "background-image":"url(../H&M/img/section2/h&m-main.jpg)",
+        "background-repeat":"no-repeat",
+        "background-size":"cover",
+        "background-position":"center",
+        "position":"relative",
+        "width: 100%":"height: 100%",
+
+
+      });
+
+
+    });
+
+// 카테고리박스03
+$('.category-box .kids').mouseover(function(){
+  $('.category-box .img-box').css({
+    "background-image":"url(../H&M/img/section2/kid.jpeg)",
+    "transition":"0.5s",
+    "background-repeat":"no-repeat",
+    "background-size":"cover",
+    "background-position":"center",
+    "position":"relative",
+    "width: 100%":"height: 100%",
+  });
+
+});
+$('.category-box .kids').mouseout(function(){
+  $('.category-box .img-box').css({
+    "background-image":"url(../H&M/img/section2/h&m-main.jpg)",
+    "background-repeat":"no-repeat",
+    "background-size":"cover",
+    "background-position":"center",
+    "position":"relative",
+    "width: 100%":"height: 100%",
+
+
+  });
+
+
+});
+      
+      // 카테고리박스04
+      $('.category-box .home').mouseover(function(){
+        $('.category-box .img-box').css({
+          "background-image":"url(../H&M/img/section2/h&m-home.jpg)",
+          "transition":"0.5s",
+          "background-repeat":"no-repeat",
+          "background-size":"cover",
+          "background-position":"center",
+          "position":"relative",
+          "width: 100%":"height: 100%",
+        });
+      
+      });
+      $('.category-box .home').mouseout(function(){
+        $('.category-box .img-box').css({
+          "background-image":"url(../H&M/img/section2/h&m-main.jpg)",
+          "background-repeat":"no-repeat",
+          "background-size":"cover",
+          "background-position":"center",
+          "position":"relative",
+          "width: 100%":"height: 100%",
+      
+      
+        });
+      
+      
+      });
+      // 카테고리박스05
+    
+      // MAGAZINE 슬라이더
+      
+      $('.magazine-slider .slider-box').slick({
+        autoplay: false,
+        slidesToShow: 1,   
+        slidesToScroll: 1,
+        fade: true,
+        pauseOnHover: true,
+        prevArrow: $(".btn-wrap1 .prev"),
+        nextArrow: $(".btn-wrap1 .next"),
+      });
 
 
 });
